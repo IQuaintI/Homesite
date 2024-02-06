63a97915c8864faf7560d9ac78477167b264fb05
quaint <alexgomez2797@gmail.com>
2024-02-06 15:01:22 -0500

    More things

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,33 @@ import "../src/css/App.scss";
 function App() {
   return (
     <>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={12} id="header">
-          <Header
-            title="Hello."
-            subtitle="I trust you are here to cause no trouble."
-          />
+      <div className="scroll-container">
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={12} id="header">
+            <Header
+              title="Hello."
+              subtitle="I trust you are here to cause no trouble."
+            />
+          </Grid>
         </Grid>
-      </Grid>
 
-      <Grid
-        id="body"
-        container
-        rowSpacing={1}
-        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-      >
-        <Grid item xs={5} id="content-left">
-          <h2>1</h2>
+        <Grid
+          id="body"
+          container
+          rowSpacing={1}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        >
+          <Grid item xs={5} id="content-left">
+            <h2>1</h2>
+          </Grid>
+          <Grid item xs={2} id="hero">
+            <Works title="Liminals" />
+          </Grid>
+          <Grid item xs={5} id="content-right">
+            <h2>2</h2>
+          </Grid>
         </Grid>
-        <Grid item xs={2} id="hero">
-          <Works title="Liminals" />
-        </Grid>
-        <Grid item xs={5} id="content-right">
-          <h2>2</h2>
-        </Grid>
-      </Grid>
+      </div>
 
       <Grid container>
         <Grid item xs={12} id="footer">
