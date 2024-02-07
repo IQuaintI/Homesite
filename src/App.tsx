158d2f541d53f28b83f02ce57dc74e665004a83f
quaint <alexgomez2797@gmail.com>
2024-02-07 17:41:55 -0500

    Completed the card component (for the most part)

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,64 @@
 import { Grid } from "@mui/material";
 import Header from "./components/Header";
 import Works from "./components/Works";
-import Footer from "./components/Footer";
 import "../src/css/App.scss";
+
+import Card from "./assets/Card";
 
 function App() {
   return (
     <>
-      <div className="scroll-container">
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item xs={12} id="header">
-            <Header
-              title="Hello."
-              subtitle="I trust you are here to cause no trouble."
-            />
+      {/* Header */}
+      <div id="scroll-container">
+        <section className="snap-section">
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            <Grid item xs={12} id="header">
+              <Header
+                title="Hello."
+                subtitle="I trust you are here to cause no trouble."
+              />
+            </Grid>
           </Grid>
-        </Grid>
+        </section>
 
-        <Grid
-          id="body"
-          container
-          rowSpacing={1}
-          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-        >
-          <Grid item xs={5} id="content-left">
-            <h2>1</h2>
+        {/* Body */}
+        <section className="snap-section">
+          <Grid
+            id="body"
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            style={{ height: "100vh" }}
+          >
+            {/* Content-Left */}
+            <Grid item xs={5} id="content-left">
+              <Card
+                image="https://via.placeholder.com/150"
+                title="lorem ipsum dolor"
+                subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              />
+            </Grid>
+
+            {/* Works */}
+            <Grid item xs={2} id="hero">
+              <Works title="Liminals" />
+            </Grid>
+
+            {/* Content-Right */}
+            <Grid item xs={5} id="content-right">
+              <Card
+                image="https://via.placeholder.com/150"
+                title="lorem ipsum"
+                subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={2} id="hero">
-            <Works title="Liminals" />
-          </Grid>
-          <Grid item xs={5} id="content-right">
-            <h2>2</h2>
-          </Grid>
-        </Grid>
+        </section>
       </div>
-
-      <Grid container>
-        <Grid item xs={12} id="footer">
-          <Footer title="Voices" />
-        </Grid>
-      </Grid>
     </>
   );
 }
